ab3fd34f41bc73c62e158257978785251cc2eb26
yk0n9 <ykonghao@icloud.com>
2024-05-24 16:35:04 +0800

    add windows-gnu

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # libredwg-rs
 
 [libredwg](https://github.com/LibreDWG/libredwg) bindings for rust
+
+current only windows
--- libredwg-sys/build.rs
+++ libredwg-sys/build.rs
@@ -1,5 +1,27 @@
+use std::process::Command;
+
+fn get_host() -> String {
+    let host = Command::new("rustc")
+        .args(["--version", "--verbose"])
+        .output()
+        .unwrap()
+        .stdout;
+    String::from_utf8(host).unwrap_or_default()
+}
+
 fn main() {
     let current = std::env::current_dir().unwrap();
-    println!("cargo:rustc-link-search={}", current.join("lib").display());
-    println!("cargo:rustc-link-lib=static=libredwg");
+    if get_host().contains("msvc") {
+        println!(
+            "cargo:rustc-link-search={}",
+            current.join("lib").join("msvc").display()
+        );
+        println!("cargo:rustc-link-lib=static=libredwg");
+    } else {
+        println!(
+            "cargo:rustc-link-search={}",
+            current.join("lib").join("gnu").display()
+        );
+        println!("cargo:rustc-link-lib=static=redwg");
+    };
 }
