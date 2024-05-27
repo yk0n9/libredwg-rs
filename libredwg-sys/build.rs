e3dea5a41d166fc998e51feb11cae4359fea016d
yk0n9 <ykonghao@icloud.com>
2024-05-27 10:44:45 +0800

    use target-triple

--- libredwg-sys/build.rs
+++ libredwg-sys/build.rs
@@ -1,25 +1,6 @@
-use std::process::Command;
-
-fn get_host() -> String {
-    let rustc_verbose = Command::new("rustc")
-        .args(["--version", "--verbose"])
-        .output()
-        .unwrap()
-        .stdout;
-    let rustc_verbose = String::from_utf8(rustc_verbose).unwrap_or_default();
-    let v = rustc_verbose
-        .trim()
-        .split(if cfg!(windows) { "\r\n" } else { "\n" })
-        .collect::<Vec<_>>();
-    v.into_iter()
-        .find(|s| s.starts_with("host"))
-        .unwrap_or_default()
-        .to_string()
-}
-
 fn main() {
     let current = std::env::current_dir().unwrap();
-    if get_host().contains("msvc") {
+    if target_triple::HOST.contains("msvc") {
         println!(
             "cargo:rustc-link-search={}",
             current.join("lib").join("msvc").display()
